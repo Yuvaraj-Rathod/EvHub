55cb4a0b0bc4636c8cae67550139e463085afcc2
Yuvaraj Rathod <yuvrajrathod0007@gmail.com>
2025-03-01 16:19:44 +0530

    initialized and created HomePage

---
+++ app/src/main/java/com/example/evhub/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.example.evhub.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkBlue = Color(0xFF202938)
+val AzureBlue = Color(0xFF0080FE)
+val DeepNavyBlue = Color(0xFF111828)
+val MidnightBlue = Color(0xFF101727)
+val CharcoalBlue = Color(0xFF212A39)
+val SlateBlue = Color(0xFF1F2837)
+val FuturisticGreen = Color(0xFF2EE59D)
+val LightGray = Color(0xFFD0D3D4)
+val White = Color(0xFFFFFFFF)
+val LightGreen = Color(0xFF00A676)
+//0080FE
---
+++ app/src/main/java/com/example/evhub/ui/theme/Theme.kt
@@ -0,0 +1,50 @@
+package com.example.evhub.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.input.nestedscroll.NestedScrollSource.Companion.SideEffect
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
+
+private val EvHubDarkColorScheme = darkColorScheme(
+    primary = FuturisticGreen,
+    onPrimary = White,
+    secondary = DarkBlue,
+    onSecondary = LightGray,
+    background = MidnightBlue,
+    onBackground = White,
+    surface = DeepNavyBlue,
+    onSurface = LightGray,
+
+)
+
+@Composable
+fun EvHubTheme(
+    content: @Composable () -> Unit
+) {
+    val view = LocalView.current
+    val systemUiController = rememberSystemUiController()
+
+    if (!view.isInEditMode) {
+        SideEffect {
+            // Use a slightly lighter dark blue for the top bar so it stands out
+            systemUiController.setStatusBarColor(color = DarkBlue)
+            // Use midnight blue for the navigation bar to match the overall background
+            systemUiController.setNavigationBarColor(color = MidnightBlue)
+        }
+    }
+    MaterialTheme(
+        colorScheme = EvHubDarkColorScheme,
+        typography = Typography,
+        content = content
+    )
+}
